c33d9200ee6e6ac6fdf674bcb7ebb0d428e4248b
Danny <yandanny2004@gmail.com>
2026-06-29 19:17:26 +1000

    Added magnetic field code, add conversion code with particle system coordinate space, add simple test of metal function

---
+++ ParticleSimulatorApp/ParticleSimulation/MetalSimulations/File.m
@@ -0,0 +1,1 @@
+float3 position
---
+++ ParticleSimulatorAppTests/ParticleSimulatorAppTests.swift
@@ -0,0 +1,19 @@
+//
+//  ParticleSimulatorAppTests.swift
+//  ParticleSimulatorAppTests
+//
+//  Created by DY on 27/6/2026.
+//  Copyright © 2026 Apple. All rights reserved.
+//
+
+import Testing
+
+struct ParticleSimulatorAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
