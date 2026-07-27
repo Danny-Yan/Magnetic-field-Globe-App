e37797e37feec1d958095e827ca0a82cc9f4b13e
Danny <yandanny2004@gmail.com>
2026-07-27 16:21:26 +1000

    Reordered testing functions

--- ParticleSimulatorAppTests/TestUtilities/GPUTester+GeomagneticCalculation.swift
+++ ParticleSimulatorAppTests/TestUtilities/GPUTester+GeomagneticCalculation.swift
@@ -15,6 +15,61 @@ import Testing
 @testable import ParticleSimulatorApp
 
 extension GPUTester {
+    
+    mutating func initialiseMetalApp() async throws {
+        AppConstants.Spawn.maxSpawnCount = 1
+        AppConstants.Spawn.minSpawnCount = 1
+        
+        AppConstants.Spawn.centre = [-0.5, 1.5, -1]
+        AppConstants.Spawn.radius = 0  // Remove Randomness from sphere position
+        
+        AppConstants.Particle.initialSpeed = 0
+        AppConstants.Particle.size = 0.3
+        
+        AppConstants.Earth.showEarth = false
+        AppConstants.Sim.skipSplashScreen = true
+        AppConstants.Sim.showSim = true
+        
+        /// The GPU command queue to store incoming GPU commands
+        let commandQueue: MTLCommandQueue? = {
+            if let metalDevice, let queue = metalDevice.makeCommandQueue() {
+                queue.label = "particle Brush Command Queue"
+                return queue
+            } else {
+                return nil
+            }
+        }()
+        
+        (coefficientBuffers, magneticModelBuffer) =
+        ParticleMeshGenerator.createModelBuffers(
+            modelCoefficients: AppConstants.modelCoefficients.igrf,
+            metalDevice: metalDevice
+        )
+        
+        magneticModelPointer = createSingleTypeBufPointer(buf: &magneticModelBuffer!, of: MagneticFieldModel.self)
+        
+        guard let commandBuf = commandQueue?.makeCommandBuffer(),
+              let compute = commandBuf.makeComputeCommandEncoder()
+        else {
+            fatalError("Command buffer failed to initalise")
+        }
+        
+        computeEncoder = compute
+        commandBuffer = commandBuf
+        commandBuffer!.enqueue()
+        
+        try? ParticleMeshGenerator.initialiseMagneticModelClass(
+            coefficientBuffers: coefficientBuffers!,
+            outputModel: magneticModelBuffer!,
+            encoder: computeEncoder!
+        )
+        
+        computeEncoder!.endEncoding()
+        commandBuffer!.commit()
+        await commandBuffer!.completed()
+    }
+    
+    
     // GPU function to call test magnetic field calc function
     private func magneticModelPipeline(
         polarCoord: SIMD3<Float>,
--- ParticleSimulatorAppTests/TestUtilities/TesterAPI.swift
+++ ParticleSimulatorAppTests/TestUtilities/TesterAPI.swift
@@ -22,60 +22,6 @@ struct GPUTester {
     var computeEncoder: MTLComputeCommandEncoder? = nil
     var commandBuffer: MTLCommandBuffer? = nil
     
-    mutating func initialiseMetalApp() async throws {
-        AppConstants.Spawn.maxSpawnCount = 1
-        AppConstants.Spawn.minSpawnCount = 1
-        
-        AppConstants.Spawn.centre = [-0.5, 1.5, -1]
-        AppConstants.Spawn.radius = 0  // Remove Randomness from sphere position
-        
-        AppConstants.Particle.initialSpeed = 0
-        AppConstants.Particle.size = 0.3
-        
-        AppConstants.Earth.showEarth = false
-        AppConstants.Sim.skipSplashScreen = true
-        AppConstants.Sim.showSim = true
-        
-        /// The GPU command queue to store incoming GPU commands
-        let commandQueue: MTLCommandQueue? = {
-            if let metalDevice, let queue = metalDevice.makeCommandQueue() {
-                queue.label = "particle Brush Command Queue"
-                return queue
-            } else {
-                return nil
-            }
-        }()
-        
-        (coefficientBuffers, magneticModelBuffer) =
-        ParticleMeshGenerator.createModelBuffers(
-            modelCoefficients: AppConstants.modelCoefficients.igrf,
-            metalDevice: metalDevice
-        )
-        
-        magneticModelPointer = createSingleTypeBufPointer(buf: &magneticModelBuffer!, of: MagneticFieldModel.self)
-        
-        guard let commandBuf = commandQueue?.makeCommandBuffer(),
-              let compute = commandBuf.makeComputeCommandEncoder()
-        else {
-            fatalError("Command buffer failed to initalise")
-        }
-        
-        computeEncoder = compute
-        commandBuffer = commandBuf
-        commandBuffer!.enqueue()
-        
-        try? ParticleMeshGenerator.initialiseMagneticModelClass(
-            coefficientBuffers: coefficientBuffers!,
-            outputModel: magneticModelBuffer!,
-            encoder: computeEncoder!
-        )
-        
-        computeEncoder!.endEncoding()
-        commandBuffer!.commit()
-        await commandBuffer!.completed()
-    }
-    
-    
     func testPipelineTemplate(encoder: MTLComputeCommandEncoder, name: String, pipeline: (_ encoder: MTLComputeCommandEncoder) -> Void){
         guard
             let testingPipeline =
